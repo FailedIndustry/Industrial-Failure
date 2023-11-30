a2e34382581c2b56e59713a558faa7d6a8fa331b
phiyerion <reidbandy@gmail.com>
2023-11-30 18:22:56 -0500

    Fix logger & abstract function & comments

--- Main.gd
+++ Main.gd
@@ -1,4 +1,5 @@
 extends Node
+@onready var main_menu = $MainMenu/CanvasLayer/MainMenu
 
 const PLAYER = preload("res://player/player.tscn")
 const Server = preload("res://multiplayer/Server.gd")
@@ -6,17 +7,9 @@ const Client = preload("res://multiplayer/Client.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Logger.set_logger_format(Logger.LOG_FORMAT_FULL)
-	
-	var datetime = Time.get_datetime_string_from_system()
-	var filelogger = Logger.add_appender(
-		FileAppender.new("res://.log/%s.log" % datetime)
-	)
-	filelogger.logger_level = Logger.LOG_LEVEL_FINE
-	
-	var consolelogger = Logger.add_appender(ConsoleAppender.new())
-	consolelogger.logger_level = Logger.LOG_LEVEL_DEBUG
 	Logger.debug("Main scene ready")
+	start_logger()
+	Logger.debug("Loggers initialized")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -27,28 +20,6 @@ func _unhandled_input(_event):
 		Logger.info("Exiting game")
 		get_tree().quit()
 
-func add_player(peer_id):
-	Logger.info("Creating player")
-	Logger.debug("Player id: %s" % peer_id)
-	
-	var player = PLAYER.instantiate()
-	
-	# this being set to peer_id is required for item's interaction check
-	# and other functions.
-	player.name = str(peer_id)
-	add_child(player)
-	Logger.info("Created Player")
-
-func remove_player(peer_id):
-	Logger.info("Removing player from scene")
-	Logger.debug("\tPlayer: %s" % peer_id)
-	var player = get_node_or_null(str(peer_id))
-	if player:
-		player.queue_free()
-	else:
-		Logger.warn("\tAttempted to remove non-existant player")
-
-@onready var main_menu = $MainMenu/CanvasLayer/MainMenu
 
 func _on_host_button_pressed():
 	var enet_peer = ENetMultiplayerPeer.new()
@@ -72,4 +43,40 @@ func _on_join_button_pressed():
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.server_disconnected.connect(main_menu.show)
 	multiplayer.connection_failed.connect(main_menu.show)
+	
+func add_player(peer_id):
+	Logger.info("Creating player")
+	Logger.debug("Player id: %s" % peer_id)
+	
+	var player = PLAYER.instantiate()
+	
+	# this being set to peer_id is required for item's interaction check
+	# and other functions.
+	player.name = str(peer_id)
+	add_child(player)
+	Logger.info("Created Player")
+
+func remove_player(peer_id):
+	Logger.info("Removing player from scene")
+	Logger.debug("\tPlayer: %s" % peer_id)
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+	else:
+		Logger.warn("\tAttempted to remove non-existant player")
+
+func start_logger():
+	Logger.logger_appenders.clear()
+	Logger.set_logger_format(Logger.LOG_FORMAT_FULL)
+	
+	# File appender
+	var datetime = Time.get_datetime_string_from_system()
+	var filelogger = Logger.add_appender(
+		FileAppender.new("res://.log/%s.log" % datetime)
+	)
+	filelogger.logger_level = Logger.LOG_LEVEL_FINE
+	
+	# Console logger
+	var consolelogger = Logger.add_appender(ConsoleAppender.new())
+	consolelogger.logger_level = Logger.LOG_LEVEL_DEBUG
 	
