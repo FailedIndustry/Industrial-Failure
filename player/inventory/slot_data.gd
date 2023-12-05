0aa129274165ee9715d9d28ef7bb056878e67df3
phiyerion <reidbandy@gmail.com>
2023-12-05 14:08:44 -0500

    More lenient error handling (no panics)

--- player/inventory/slot_data.gd
+++ player/inventory/slot_data.gd
@@ -9,12 +9,12 @@ const MAX_STACK_SIZE: int = 99
 func set_quantity(value: int):
 	quantity = value
 	if quantity > 1 and not item_data.stackable:
-		Logger.error("slot_data.set_quantity: Adding more than 1 to item_data")
+		Logger.error("slot_data.set_quantity: Adding more than 1 to item_data \
+					  when not stackable. Adding one to preserve total quantity")
 		push_error("slot_data.set_quantity: %s is stacking but not stackable" 
 				  % item_data.name)
 	elif quantity < 0:
 		Logger.fatal("slot_data.set_quantity: quantity is less than 0. \
 					  There is most likely a duplication glitch somewhere.")
 		push_error("QUANTITY LESS THAN 0 NOT ALLOWED >:(")
-		assert(0 == 1)
-
+		Logger.fatal("Continuing due to lack of ability to rectify this issue.")
