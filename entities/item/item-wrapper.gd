11233c1d78dd925007d94a9228f646fe8051d59d
phiyerion <reidbandy@gmail.com>
2023-12-11 19:50:13 -0500

    files needed

---
+++ entities/item/item-wrapper.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name ItemWrapper
+
+@export var quantity: int = 1 : set = set_quantity
+@export var item_type: ItemType
+
+func set_quantity(value: int) -> void:
+	if value < 0:
+		Logger.error("ItemData.set_quantity: quantity %s < 0" % value)
+	quantity = value
+
+## This should be changed to owner_id or something like that
+var owner: Object
