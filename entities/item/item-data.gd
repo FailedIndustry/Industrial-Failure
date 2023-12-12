9df3846a902b0443484cd14f42798ebc457b1582
phiyerion <reidbandy@gmail.com>
2023-12-11 19:16:28 -0500

    Add setter

--- entities/item/item-data.gd
+++ entities/item/item-data.gd
@@ -7,7 +7,12 @@ class_name ItemData
 @export var texture: Texture
 
 ## This needs to be abstracted out
-@export var quantity: int = 1
+@export var quantity: int = 1 : set = set_quantity
+
+func set_quantity(value: int) -> void:
+	if value < 0:
+		Logger.error("ItemData.set_quantity: quantity %s < 0" % value)
+	quantity = value
 
 ## This should be changed to owner_id or something like that
 var owner: Object
