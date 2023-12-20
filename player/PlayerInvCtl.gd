5dc97efa37b385a4cac0e110f42d170e57d8e266
PhiYerion <reidbandy@gmail.com>
2023-12-20 00:29:02 -0500

    Better inventory abstraction and params

--- player/PlayerInvCtl.gd
+++ player/PlayerInvCtl.gd
@@ -6,9 +6,10 @@ class_name WICtl
 ## Unlike Player class that has no RPCs, WICtl is inherantly networked and handles networking
 ## functions for the player.
 
-@export var inventory: Inventory
+@export var inventory: Inventory = Inventory.new()
 @export var INTERACTION_DISTANCE = 2
-@onready var server_global: ServerGlobal
+@onready var server_global: ServerGlobal = get_node("/root/ServerGlobal")
+@onready var global: Globals = get_node("/root/Globals")
 
 ## Variable to determine if a drop has completed. In [method drop_item], it gets set to -1 for not
 ## complete. In [method _spawn_item], it is set to the id of the item for complete.
@@ -19,7 +20,6 @@ func _ready():
 	var parent = get_parent()
 	if parent is Player:
 		player = parent
-		server_global = player.get_node("/root/ServerGlobal")
 	else:
 		Logger.error("WICtl: parent node must be player, but found %s" % parent)
 
@@ -33,19 +33,16 @@ func drop_item(item: ItemWrapper):
 ## Drops the item passed in. Can be a fraction of what the player actually has.
 ## In the case of error, -1 is returned
 @rpc ("reliable","any_peer","call_local") 
-func _server_drop_item(item_id:int, type_id: int, quantity: int) -> int:
+func _server_drop_item(item_id: int, type_id: int, quantity: int) -> int:
 	push_error("server")
 	Logger.debug("%s" % multiplayer.get_remote_sender_id())
 	Logger.debug("%s" % multiplayer.get_peers())
 	Logger.debug("%s" % multiplayer.get_unique_id())
-	var item = ItemWrapper.new()
-	item.id = item_id
-	item.quantity = quantity
-	item.item_type = server_global.item_types[type_id]
-	Logger.debug("wictl._server_drop_item: Dropping %d %s" % [item.quantity, item.item_type.name])
-	if inventory.remove(item, item.id) != 0: return -1
-	item.owner = get_node("/root/Main/Game")
-	if _server_create_item(item) != 0: return -1
+	var type = server_global.item_types[type_id]
+	Logger.debug("wictl._server_drop_item: Dropping %d %s" % [quantity, type.name])
+	var new_item = inventory.remove(type, quantity, item_id)
+	new_item.owner = get_node("/root/Main/Game")
+	if _server_create_item(new_item) != 0: return -1
 	
 	return 0
 
